1ac7aebe061b53c1989a8eee0e3f5532643da560
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-06-29 13:17:42 +0800

    feat(auth): implement password hashing for login and signup

--- backend/services/auth.js
+++ backend/services/auth.js
@@ -2,21 +2,22 @@ const mysql = require('mysql2/promise')
 const express = require('express')
 const pool = require('../db')
 const router = express.Router()
+const { generateAccessToken, generateRefreshToken } = require('../utils/generateTokens.js')
+const bcrypt = require('bcrypt')
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body
-    const loginInfo = {
-        username: username,
-        password: password
-    }
-    if (!loginInfo || !loginInfo?.username || !loginInfo?.password) return res.status(400).json({ message: "Invalid username or password" })
+    if (!username || !password) return res.status(400).json({ message: "Invalid username or password" })
     try {
         const query = `SELECT * FROM user_tbl WHERE username = ? LIMIT 1 `
-        const [row] = await pool.execute(query, [loginInfo.username])
-        const user = row[0]
-
-        if (!user || loginInfo.password !== user.hashed_password) return res.status(400).json({ message: "Invalid username or password" })
-
+        const [rows] = await pool.execute(query, [username])
+        const user = rows[0]
+        console.log('incoming error for:', user)
+        if (!user) return res.status(401).json({ message: "User no longer exist" })
+        console.log('user', user)
+        const isValid = await bcrypt.compare(password, user.hashed_password)
+        if (!isValid) return res.status(401).json({ message: 'Invalid username or password' })
+        console.log('valid')
         return res.status(200).json({ user: user, status: 'ok' })
     } catch (e) {
         return res.status(500).json({ message: e.message })
@@ -26,11 +27,12 @@ router.post('/login', async (req, res) => {
 router.post('/signup', async (req, res) => {
     const { username, password } = req.body
     console.log(username, password)
-    if (!username || !password) return res.status(400).json({ message: 'Username and Password must not be empty' })
+    if (!username || !password) return res.status(400).json({ message: 'Invalid username or password' })
 
     try {
+        const hashedPassword = await bcrypt.hash(password, 12)
         const sql = `INSERT INTO user_tbl(username, hashed_password) values (?, ?)`
-        const values = [username, password]
+        const values = [username, hashedPassword]
         await pool.execute(sql, values)
         return res.status(201).json({ message: 'Account successfully created', status: 'ok' })
     } catch (e) {
