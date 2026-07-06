721832f03e64d7b75336e864dd330a1b696e7d79
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-07-06 16:36:19 +0800

    feat(chatbubble): implement chat messages grouping
    To display chat messages that have been sent together at a short time span

--- chatara-frontend/src/pages/chatroom/ChatBubble/ChatBubble.jsx
+++ chatara-frontend/src/pages/chatroom/ChatBubble/ChatBubble.jsx
@@ -4,47 +4,57 @@ import pfpImage from '/icons/pfp.svg'
 
 export default function ChatBubble({ chatMessage, prevChatMessage, nextChatMessage }) {
     const { user } = useAuth()
-    const { chatStyle, chatBubble, chatInfo, imageContainerStyle, usernameStyle, sent, received, firstRecentChat, recentlyReceived } = style
-    const isSender = chatMessage.sender_id === user?.id ? true : false
+    const { chatStyle, chatBubble, chatInfo, imageContainerStyle, usernameStyle, sent, received, firstRecentChat, recentlyReceived, partOfRecentMessageGroupStyle, lastRecentChatStyle } = style
+    
+    const isSender = chatMessage.sender_id === user?.id
     const fiveMins = 300000
+    const currentChatMessageSentAtMs = new Date(chatMessage.sent_at)
+    const currentChatMessageSenderId = chatMessage.sender_id
+    const prevChatMessageSentAtMs = new Date(prevChatMessage?.sent_at)
+    const prevChatMessageSenderId = prevChatMessage?.sender_id
+    const nextChatMessageSentAtMs = new Date(nextChatMessage?.sent_at)
+    const nextChatMessageSenderId = nextChatMessage?.sender_id
 
-    const fromSameSender = () => {
-        if (nextChatMessage && nextChatMessage.sender_id !== user.id && nextChatMessage.sender_id === chatMessage.sender_id) {
-            return checkIfRecent(true)
-        }
-        return false
+    function isRecent(recent, old) {
+        const lapsedTimeMs = recent - old
+        if (lapsedTimeMs > fiveMins) return false
+        return true
     }
 
-    const checkIfRecent = (bool) => {
-        if (!bool) return false
+    function checkIfFirstMessageGroup() {
+        if (!nextChatMessage || nextChatMessageSenderId !== currentChatMessageSenderId) return false
 
-        const lapsedTimeInMs = new Date(nextChatMessage.sent_at) - new Date(chatMessage.sent_at)
-        if (lapsedTimeInMs < fiveMins) return true
-        return false
+        const isPrevInGroup = isRecent(currentChatMessageSentAtMs, prevChatMessageSentAtMs)
+        if (isPrevInGroup) return false
+
+        const isNextInGroup = isRecent(nextChatMessageSentAtMs, currentChatMessageSentAtMs)
+        if (!isNextInGroup) return false
+
+        return true
     }
 
-    const checkIfFirst = () => {
-        if (!prevChatMessage && chatMessage.sender_id !== user.id || prevChatMessage && prevChatMessage.sender_id !== chatMessage.sender_id && prevChatMessage.sender_id === user.id) return true
-        if (nextChatMessage && nextChatMessage.sender_id !== user.id && nextChatMessage.sender_id === chatMessage.sender_id) {
-            const firstLapsedTimeInMs = new Date(nextChatMessage.sent_at) - new Date(chatMessage.sent_at)
-            const lastLapsedTimeInMs = new Date(chatMessage.sent_at) - new Date(prevChatMessage.sent_at)
-            if (firstLapsedTimeInMs < fiveMins && lastLapsedTimeInMs > fiveMins) return true
-            return false
-        }
-        return false
+    function checkIfPartOfRecentMessageGroup() {
+        if (!prevChatMessage || !nextChatMessage || prevChatMessageSenderId !== currentChatMessageSenderId || nextChatMessageSenderId !== currentChatMessageSenderId) return false
+        return isRecent(nextChatMessageSentAtMs, currentChatMessageSentAtMs)
     }
 
-    const isReceivedRecent = fromSameSender()
-    const isFirst = checkIfFirst()
+    function checkIfLastOfMessageGroup() {
+        if (!prevChatMessage || prevChatMessageSenderId !== currentChatMessageSenderId) return false
+        return isRecent(currentChatMessageSentAtMs, prevChatMessageSentAtMs)
+    }
+
+    const isFirst = checkIfFirstMessageGroup()
+    const isReceivedRecent = checkIfPartOfRecentMessageGroup()
+    const isLast = checkIfLastOfMessageGroup()
 
     return (
-        <div className={`${chatStyle} ${isSender ? sent : received} ${isReceivedRecent && recentlyReceived}`}>
-            <div className={imageContainerStyle} hidden={isSender || isReceivedRecent}>
+        <div className={`${chatStyle} ${isSender ? sent : received} ${isReceivedRecent && recentlyReceived || isFirst && recentlyReceived}`}>
+            <div className={imageContainerStyle} hidden={isSender || isReceivedRecent || isFirst}>
                 <img src={pfpImage} alt="Profile picture" />
             </div>
             <div className={`${chatInfo}`}>
                 <p className={usernameStyle} hidden={isSender || !isFirst} >{chatMessage.sender_name}</p>
-                <div className={`${chatBubble} ${isFirst && firstRecentChat}`} >
+                <div className={`${chatBubble} ${isFirst && firstRecentChat} ${isReceivedRecent && partOfRecentMessageGroupStyle} ${isLast && lastRecentChatStyle} `} >
                     <p>{chatMessage.message_text}</p>
                 </div>
             </div>
