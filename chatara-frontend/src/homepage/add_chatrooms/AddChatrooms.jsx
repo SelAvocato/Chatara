a994dd0ac501c92c52302fbf2c94b4369c455842
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-07-18 15:36:20 +0800

    refactor(frontend): remove old homepage folder

--- chatara-frontend/src/homepage/add_chatrooms/AddChatrooms.jsx
+++
@@ -1,8 +0,0 @@
-export default function AddChatrooms(){
-    return(
-        <div>
-            <label htmlFor="chatroom">Group name:</label>
-            <input type="text" name="chatroom" />
-        </div>
-    )
-}
