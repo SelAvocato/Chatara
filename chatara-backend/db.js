eeca0cc2fc79e43e76336fe61acbef37b92aa7a5
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-07-18 11:58:06 +0800

    refactor(db): replace hard-coded db info with environment variables

--- chatara-backend/db.js
+++ chatara-backend/db.js
@@ -1,12 +1,21 @@
+require('dotenv/config')
 const mysql = require('mysql2/promise')
+const host = process.env.DB_HOST
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const database = process.env.DB_NAME
+const waitForConnections = process.env.DB_WAIT_FOR_CONNECTION
+const connectionLimit = Number(process.env.DB_CONNECTION_LIMIT)
 
+console.log('user', user)
+console.log('db password', password, typeof (password))
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "messenger_db",
-    waitForConnections: true,
-    connectionLimit: 10
+    host: host,
+    user: user,
+    password: password,
+    database: database,
+    waitForConnections: waitForConnections,
+    connectionLimit: connectionLimit
 })
 
 module.exports = pool
