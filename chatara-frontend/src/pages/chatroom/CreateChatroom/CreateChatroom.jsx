aabe1438f98347dd56a91eb36ee1aaf0f7d86a51
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-07-19 10:25:37 +0800

    feat(createchatroom): design the ui for create chatroom modal

--- chatara-frontend/src/pages/chatroom/CreateChatroom/CreateChatroom.jsx
+++ chatara-frontend/src/pages/chatroom/CreateChatroom/CreateChatroom.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useApi } from "../../../hooks/useApi"
 import { useAuth } from "../../../hooks/useAuth"
 import style from "./CreateChatroom.module.css"
+import userProfile from '/icons/pfp.svg'
 
 export default function CreateChatroom({ setIsCreatingChatroom }) {
     const api = useApi()
     const { user } = useAuth()
-    const { formContainer, closeBtnStyle } = style
+    const { formContainer, closeBtnStyle, formStyle, chatroomNameStyle, memberContainerStyle, userProfileContainerStyle, userRowStyle, buttonsContainerStyle,
+        usernameStyle } = style
+    const focusLastMember = useRef(null)
     const [errorMessage, setErrorMessage] = useState(null)
     const [memberCount, setMemberCount] = useState(1)
+
+    useEffect(() => {
+        const lastMemberContainer = focusLastMember.current
+        if (!lastMemberContainer) return
+        lastMemberContainer.scrollIntoView({ behavior: 'smooth' })
+    }, [memberCount])
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -35,27 +44,35 @@ export default function CreateChatroom({ setIsCreatingChatroom }) {
     }
     return (
         <div className={formContainer}>
-            <button className={closeBtnStyle} onClick={() => setIsCreatingChatroom(false)}>x</button>
-            <form onSubmit={handleSubmit}>
-                <input name='chatroomName' type="text" placeholder='Chatroom Name' />
-                <div>
-                    {
-                        Array.from({ length: memberCount }).map((_member, i) =>
-                            <div key={i}>
-                                <input name='username' type="text" placeholder='Participant Name' />
-                            </div>
-                        )
-                    }
-                    <input type="button" onClick={() => setMemberCount(prev =>
-                        memberCount === 1
-                            ? prev
-                            : prev - 1
-                    )} value={'x'} />
-                    <input type="button" onClick={() => setMemberCount(prev => prev + 1)} value={'+'} />
-                </div>
-                <input type="submit" />
-                {errorMessage && errorMessage}
-            </form>
+            <div className={formStyle}>
+                <button className={closeBtnStyle} onClick={() => setIsCreatingChatroom(false)}>x</button>
+                <form onSubmit={handleSubmit}>
+                    <input className={chatroomNameStyle} name='chatroomName' type="text" placeholder='Chatroom Name' autoComplete='off' />
+                    <div className={memberContainerStyle}>
+                        {
+                            Array.from({ length: memberCount }).map((_member, i) =>
+                                <div className={userRowStyle} key={i} ref={focusLastMember}>
+                                    <div className={userProfileContainerStyle}>
+                                        <img src={userProfile} alt="User Profile" />
+                                    </div>
+                                    <input className={usernameStyle} name='username' type="text" placeholder='Participant Name' autoComplete='off' />
+                                    {i === memberCount - 1 &&
+                                        <div className={buttonsContainerStyle}>
+                                            <input type="button" onClick={() => setMemberCount(prev =>
+                                                memberCount === 1
+                                                    ? prev
+                                                    : prev - 1
+                                            )} value={'x'} hidden={memberCount === 1} />
+                                            <input type="button" onClick={() => setMemberCount(prev => prev + 1)} value={'+'} />
+                                        </div>}
+                                </div>
+                            )
+                        }
+                    </div>
+                    {errorMessage && <p>{errorMessage}</p>}
+                    <input type="submit" value={'Create'} />
+                </form>
+            </div>
         </div>
     )
 }
