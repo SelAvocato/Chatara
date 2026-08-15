144051abc6fa9ab34f0bfe43fecc109677360678
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-08-15 21:32:13 +0800

    fix(auth): allow cross-origin cookies during production

--- chatara-backend/services/auth.js
+++ chatara-backend/services/auth.js
@@ -10,7 +10,7 @@ const { authenticate } = require('../middleware/authenticate.js')
 const refreshTokenOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000
 }
 
